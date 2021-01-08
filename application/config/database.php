b8e1039c04ce573b5035b76cb5e09682543f9bbe
vadgun <vadgun@hotmail.com>
2021-01-08 12:46:08 -0600

    prueba

--- application/config/database.php
+++ application/config/database.php
@@ -76,9 +76,9 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => 'publihom_root',
-	'password' => '&=FxE*j.x.Hp',
-	'database' => 'publihom_sivem',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'sivem',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
